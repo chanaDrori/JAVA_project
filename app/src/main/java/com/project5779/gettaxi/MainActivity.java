3ff2c780cf8d79faa29afaf76dfd39fde0e32a1e
israel zipiilevich <hanazipi15@gmail.com>
2018-10-16 14:46:24 +0300

    hello

---
+++ app/src/main/java/com/project5779/gettaxi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.project5779.gettaxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
