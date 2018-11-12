dfa1bde8252181a9292999d50485c8a19c9ad0f5
israel zipiilevich <hanazipi15@gmail.com>
2018-11-12 19:57:28 +0200

    19:55

---
+++ app/src/main/java/com/project5779/gettaxi/model/datasource/DatabaseFirebase.java
@@ -0,0 +1,17 @@
+package com.project5779.gettaxi.model.datasource;
+
+import com.project5779.gettaxi.model.backend.DBmanager;
+import com.project5779.gettaxi.model.entities.Drive;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DatabaseFirebase implements DBmanager{
+    @Override
+    public void addNewDrive(Drive newDrive) throws Exception {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
