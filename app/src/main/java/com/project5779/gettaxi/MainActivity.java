a4bd328c844f1fb8bcd532db65f5aae715581ca1
israel zipiilevich <hanazipi15@gmail.com>
2018-11-12 12:35:37 +0200

    12:35

--- app/src/main/java/com/project5779/gettaxi/MainActivity.java
+++ app/src/main/java/com/project5779/gettaxi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.project5779.gettaxi;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b2 =(Button) findViewById(R.id.button2);
+        b2.setBackgroundColor(Color.YELLOW);
+
+        Button b3 =(Button) findViewById(R.id.button3);
+        b3.setBackgroundColor(Color.YELLOW);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //new activity
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //new activity
+            }
+        });
+
     }
 }
